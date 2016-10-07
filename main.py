2517f283edebba11581b509607cd1dab5f0b0d87
icguy <ayhandani@hotmail.com>
2016-10-07 22:00:49 +0200

    reverted old main

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import os
 from os.path import join
 import pygame
-import cv2
-import numpy as np
-from DiffManager import DiffManager
-from preproc import *
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 IMG_DIR = join(ROOT_DIR, "images")
@@ -12,26 +8,31 @@ IMG_DIR = join(ROOT_DIR, "images")
 pygame.init()
 img1 = pygame.image.load(join(IMG_DIR, "img1_1.jpg"))
 img2 = pygame.image.load(join(IMG_DIR, "img1_2.jpg"))
-img1 = pygame.image.load(join(IMG_DIR, "img2_1.png"))
-img2 = pygame.image.load(join(IMG_DIR, "img2_2.png"))
+img_mask = pygame.image.load(join(IMG_DIR, "img1_mask.png"))
 img_w, img_h = img1.get_rect().size
 canvas = pygame.Surface((img_w * 2, img_h))
 screen = pygame.display.set_mode((img_w * 2, img_h))
 img1 = img1.convert()
 img2 = img2.convert()
-mask = cv2.imread(join(IMG_DIR, "img2_mask.png"), 0)
-print "bboxes"
-indices, newidx = indexObjects(mask)
-bboxes = boundingBoxes(indices)
-print "bboxes done"
-dm = DiffManager(img1, img2, bboxes, canvas, (0, 0), (img_w, 0))
+img_mask = img_mask.convert()
 canvas = canvas.convert()
-canvas.fill((255, 128, 0))
 done = False
+# is_blue = True
+# is_img = False
+# x = 30
+# y = 30
+
+rect_dict = {
+    1 : pygame.Rect(30, 187, 48, 53),
+    2 : pygame.Rect(92, 173, 64, 84),
+    3 : pygame.Rect(411, 92, 83, 102),
+    4 : pygame.Rect(443, 255, 53, 51),
+    5 : pygame.Rect(510, 357, 52, 50)
+}
 
 clock = pygame.time.Clock()
-dm.draw()
-screen.blit(canvas, (0, 0))
+canvas.blit(img1, (0, 0))
+canvas.blit(img2, (img_w, 0))
 
 found_list = set()
 
@@ -45,14 +46,24 @@ while not done:
             break
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            loc = pygame.mouse.get_pos()
-            dm.click(loc)
-            if len(dm.bboxes) == 0:
-                done = True
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            mouse_x %= img_w
+            mask_color = img_mask.get_at((mouse_x, mouse_y))
+            idx = mask_color[2]
+            print mask_color, idx
+            if idx != 0:
+                found_list.add(idx)
+                if(len(found_list)) == 5:
+                    done = True
+                    break
+                    
+                rect = rect_dict[idx]
+                print rect, rect.topleft
+                canvas.blit(img2, rect.topleft, rect)
 
-    dm.draw()
-    screen.fill((255, 128, 0))
-    screen.blit(dm.canvas, (0, 0))
+    screen.fill((0, 0, 0))
+
+    screen.blit(canvas, (0, 0))
 
     pygame.display.flip()
     clock.tick(60)
