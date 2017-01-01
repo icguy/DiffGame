5103e2901487d0014582f7e6502741c4fa50dc93
icguy <ayhandani@hotmail.com>
2017-01-01 15:43:51 +0100

    fixed multiple import bug in level_data.py

--- level_data.py
+++ level_data.py
@@ -1,6 +1,5 @@
 from os import listdir
 from os.path import splitext
-import main
 
 def get_names(img1):
     basename, ext = splitext(img1)
@@ -10,6 +9,10 @@ def get_names(img1):
             "%s%s%s" % (basename, "_mask", ext))
 
 def get_levels():
+    import main
+    """
+    :return: list of 3-tuples: ("level_1.png", "level_2.png", "level_mask.png")
+    """
     return [get_names(img) for img in listdir(main.IMG_DIR) if splitext(img)[0].endswith("_1")]
 
 if __name__ == '__main__':
